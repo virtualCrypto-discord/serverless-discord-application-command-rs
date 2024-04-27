fdcf0e10a05509dde72d08795e5b20ed1d3386a3
tignear <tignear+m@gmail.com>
2024-04-27 22:21:44 +0900

    feat: rest config

--- crates/serverless-discord-interactions/src/lib.rs
+++ crates/serverless-discord-interactions/src/lib.rs
@@ -2,12 +2,31 @@ pub use serverless_discord_interactions_base::rest;
 
 use serverless_discord_interactions_base::rest::AuthPrefix;
 pub(crate) use serverless_discord_interactions_cloudflare_backend as backend;
+const DEFAULT_API: &str = "https://discord.com/api";
+const DEFAULT_VERSION: &str = "10";
 
-pub fn rest_client<'a>(
-    api: &'a str,
-    version: &'a str,
-    token: Option<&'a str>,
-    auth_prefix: AuthPrefix,
-) -> impl rest::REST + 'a {
+pub struct RESTConfig<'a> {
+    pub api: &'a str,
+    pub version: &'a str,
+    pub token: Option<&'a str>,
+    pub auth_prefix: AuthPrefix,
+}
+impl<'a> RESTConfig<'a> {
+    pub fn new(token: &'a str) -> Self {
+        Self {
+            api: DEFAULT_API,
+            version: DEFAULT_VERSION,
+            token: Some(token),
+            auth_prefix: AuthPrefix::Bot,
+        }
+    }
+}
+pub fn rest_client<'a>(config: RESTConfig<'a>) -> impl rest::REST + 'a {
+    let RESTConfig {
+        api,
+        version,
+        token,
+        auth_prefix,
+    } = config;
     backend::REST::new(api, version, token, auth_prefix)
 }
