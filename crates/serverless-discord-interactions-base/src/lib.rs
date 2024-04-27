457568bf8eaafd03890d6e8d4daeebc1e846e75b
tignear <tignear+m@gmail.com>
2024-04-27 22:12:57 +0900

    fix: remove unused code

--- crates/serverless-discord-interactions-base/src/lib.rs
+++ crates/serverless-discord-interactions-base/src/lib.rs
@@ -1,8 +1,3 @@
-use interaction::InteractionHandler;
-
 pub mod interaction;
 pub mod rest;
 pub use twilight_model;
-pub trait Service {
-    fn register_handler(handler: impl InteractionHandler);
-}
